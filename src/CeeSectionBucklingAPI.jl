fb0895fd34dbf0d08f2e9b10966055165de431e5
Cris Moen <cris.moen@runtosolve.com>
2026-03-24 18:03:48 -0400

    get going

---
+++ src/CeeSectionBucklingAPI.jl
@@ -0,0 +1,62 @@
+module CeeSectionBucklingAPI
+
+using JSON 
+
+struct Inputs
+
+    E
+    ν
+
+    t 
+
+    L   #lip
+    B   #flange
+    H   #web
+    r   #inside radius 
+
+end
+
+
+struct Outputs 
+
+    Pcrℓ
+    Pcrd 
+
+end
+
+
+function perform_calculation(inputs_path, serial_path)
+
+    inputs = open(inputs_path) do f; JSON.parse(f); end
+
+    E = inputs.E 
+    ν = inputs.ν
+    t = inputs.t 
+    L = inputs.L 
+    B = inputs.B 
+    H = inputs.H 
+    r = inputs.r 
+
+
+    material = CeeSectionBuckling.Material(E, ν)
+    dimensions = CeeSectionBuckling.Dimensions(t, L, B, H, r)
+
+    all_results = []
+
+    push!(all_results, CeeSectionBuckling.calculate_Pcrℓ(dimensions, material))
+    push!(all_results, CeeSectionBuckling.calculate_Pcrd(dimensions, material))
+
+
+    outputs = Outputs(all_results[1].results.Rcr, all_results[2].results.Rcr)
+
+    open(serial_path, "w") do f
+        JSON.json(f, outputs)
+        println(f)
+    end
+
+end
+
+
+
+
+end # module CeeSectionBucklingAPI
---
+++ test/api_test.jl
@@ -0,0 +1,77 @@
+using JSON 
+
+struct Inputs
+
+
+    E
+    ν
+
+    t 
+
+    L   #lip
+    B   #flange
+    H   #web
+    r   #inside radius 
+
+end
+
+
+struct Outputs 
+
+    Pcrℓ
+    Pcrd 
+
+end
+
+E = 29500.0
+ν = 0.30 
+
+t = 0.060
+L = 3/8
+B = 1.0
+H = 2.0
+r = 2 * t
+
+inputs = Inputs(E, ν, t, L, B, H, r)
+
+
+JSON.json(joinpath(@__DIR__, "inputs.json"), inputs)
+
+
+
+inputs_path = joinpath(@__DIR__, "inputs.json")
+
+serial_path = joinpath(@__DIR__, "outputs.json")
+
+inputs = open(inputs_path) do f; JSON.parse(f); end
+
+E = inputs.E 
+ν = inputs.ν
+t = inputs.t 
+L = inputs.L 
+B = inputs.B 
+H = inputs.H 
+r = inputs.r 
+
+
+material = CeeSectionBuckling.Material(E, ν)
+dimensions = CeeSectionBuckling.Dimensions(t, L, B, H, r)
+
+all_results = []
+
+
+
+push!(all_results, CeeSectionBuckling.calculate_Pcrℓ(dimensions, material))
+push!(all_results, CeeSectionBuckling.calculate_Pcrd(dimensions, material))
+
+
+outputs = Outputs(all_results[1].results.Rcr, all_results[2].results.Rcr)
+
+open(serial_path, "w") do f
+    JSON.json(f, outputs)
+    println(f)
+end
+
+
+
+
